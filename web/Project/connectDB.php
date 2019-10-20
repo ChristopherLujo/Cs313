2afe95817f43ef828855fb98a0053ba366dfd4aa
unknown <luj17001@byui.edu>
2019-10-19 21:43:13 -0600

    DBConnect still fixing

--- web/Project/connectDB.php
+++ web/Project/connectDB.php
@@ -1,16 +1,12 @@
 <?php
 function get_db() {
-	$db = NULL;
+
 	try {
 
 		$dbUrl = getenv('DATABASE_URL');
-		if (!isset($dbUrl) || empty($dbUrl)) {
-		
-			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
-		
-		}
-	
-		$dbopts = parse_url($dbUrl);
+
+        $dbopts = parse_url($dbUrl);
+        
 		$dbHost = $dbopts["host"];
 		$dbPort = $dbopts["port"];
 		$dbUser = $dbopts["user"];
@@ -18,13 +14,13 @@ function get_db() {
 		$dbName = ltrim($dbopts["path"],'/');
 		// Create the PDO connection
 		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+        return $db;
     }
     
 	catch (PDOException $ex) {
 		echo "Error connecting to DB. Details: $ex";
 		die();
 	}
-	return $db;
 }
 ?>
